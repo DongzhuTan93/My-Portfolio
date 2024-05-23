55c44dce5a774072a714efd595d859c7fd4aeb74
Dongzhu Tan <dt222ha@student.lnu.se>
2024-05-23 14:27:59 +0200

    Changed log in forms layout

--- my-next-app/src/pages/login.jsx
+++ my-next-app/src/pages/login.jsx
@@ -1,5 +1,3 @@
-// import { useState } from 'react'
-// import { FormEvent } from 'react'
 import { useRouter } from 'next/router'
 
  
@@ -27,69 +25,40 @@ function LoginPage() {
   }
  
   return (
-    <form onSubmit={handlelogin}>
-      <input type="email" name="email" placeholder="Email" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <button type="submit">Login</button>
-    </form>
-  )
-
-}
-
-export default LoginPage
-
-
-
-
-  /*const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log('Username:', username)
-    console.log('Password:', password)
-  };
-
-  return (
     <div className="flex justify-center items-start h-screen bg-gray-100">
       <div className="w-full max-w-2xl  bg-white shadow-lg rounded-lg p-10 mt-40">
-        <h2 className="text-6xl font-bold mb-10 text-center">Login</h2>
-        <form onSubmit={handleLogin}>
-          <div className="mb-10">
-            <label htmlFor="username" className="text-2xl block text-gray-700 font-bold mb-2">Username</label>
+        <h2 className="text-6xl font-bold mb-10 text-center text-blue-500 ">Login</h2>
+        <form onSubmit={handlelogin}>
+          <div className="mb-10 py-3">
+            <label htmlFor="email" className="text-2xl block text-gray-700 font-bold mb-2">Email</label>
             <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="email"
+              placeholder="Email"
+              className="w-full px-4 py-3 text-2xl border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
-          <div className="mb-10">
-            <label htmlFor="password" className="text-2xl block text-gray-700 font-bold mb-2">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
+          <div className="mb-10 py-3">
+              <label htmlFor="password" className="text-2xl block text-gray-700 font-bold mb-2">Password</label>
+              <input
+                type="password"
+                placeholder="Email"
+                className="w-full px-4 py-3 text-2xl border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
           </div>
-          <button
-            type="submit"
-            className="text-2xl w-full bg-blue-500 text-white font-bold py-3 rounded-full hover:bg-blue-700 focus:outline-none"
-          >
+          <button type="submit" className="text-2xl w-full bg-blue-500 text-white font-bold py-3 rounded-full hover:bg-blue-700 focus:outline-none">
             Login
           </button>
         </form>
       </div>
     </div>
   )
+
 }
 
-export default LoginPage*/
+export default LoginPage
+
 
 // https://tailwindcss.com/docs/ring-color
 // https://tailwindcss.com/docs/border-radius
